4938ad882a00980001b9116b1813c9539b6b5643
joseju <57201000026@utrng.edu.mx>
2022-02-26 19:48:03 -0600

    Insert users

--- sql/setup.sql
+++ sql/setup.sql
@@ -4,9 +4,18 @@ CREATE DATABASE contacts_app;
 
 USE contacts_app;
 
-CREATE TABLE contacts (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
+ 
+INSERT INTO users (name, email, password) VALUES ("joseju", "josejugamer@gmail.com", "holahola");
+
+CREATE TABLE contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE,
     phone_number VARCHAR(255)
 );
 
